3dda8f0960af9b6cc4ef96dac9806ba54b01b95b
Greg Chadwick <gac@lowrisc.org>
2023-01-12 12:41:01 +0000

    [sw] Refactor SPI driver to use DEV_READ/DEV_WRITE

--- sw/common/spi.h
+++ sw/common/spi.h
@@ -10,12 +10,9 @@
 
 #define SPI_STATUS_TX_FULL 1 << 0
 #define SPI_STATUS_TX_EMPTY 1 << 1
-#define SPI_FROM_BASE_ADDR(addr)((spi_reg_t*)(addr))
+#define SPI_FROM_BASE_ADDR(addr)((spi_reg_t)(addr))
 
-typedef struct spi_reg{
-    uint32_t tx_reg;
-    uint32_t status_reg;
-}spi_reg_t;
+typedef void* spi_reg_t;
 
 typedef enum{
     spi_status_fifo_full = SPI_STATUS_TX_FULL,
@@ -23,11 +20,11 @@ typedef enum{
 }spi_status_t;
 
 typedef struct spi{
-    spi_reg_t *reg;
+    spi_reg_t reg;
     uint32_t speed;
 }spi_t;
 
-void spi_init(spi_t *spi, spi_reg_t *reg, uint32_t speed);
+void spi_init(spi_t *spi, spi_reg_t reg, uint32_t speed);
 void spi_send_byte_blocking(spi_t *spi, char c);
 spi_status_t spi_get_status(spi_t *spi);
 
