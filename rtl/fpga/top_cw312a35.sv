b9ac6a3ec5694ceb6a27939109a1e4e68a094c1c
jpcrypt <jpthibault@newae.com>
2023-03-29 14:08:26 -0400

    Tweak CW305/312 targets for use with ChipWhisperer

--- rtl/fpga/top_cw312a35.sv
+++ rtl/fpga/top_cw312a35.sv
@@ -5,34 +5,41 @@
 // This is the top level SystemVerilog file that connects the IO on the board to the Ibex Demo System.
 module top_cw312a35 (
   // These inputs are defined in data/pins_cw305.xdc
-  input               IO_CLK,
-  input               IO_RST_N,
-  input  [ 1:0]       SW,
-  output [ 2:0]       LED,
-  output [11:0]       RGB_LED,
-  input               UART_RX,
-  output              UART_TX
+  input  logic          IO_CLK,
+  input  logic          IO_RST_N,
+  input  logic          IO3,
+  output logic          IO4,
+  output logic [ 2:0]   LED,
+  input  logic          UART_RX,
+  output logic          UART_TX
 );
   parameter SRAMInitFile = "";
 
   logic clk_sys, rst_sys_n;
+  reg [24:0] clock_heartbeat;
+
+  assign LED[0] = clock_heartbeat[24];
+  assign LED[1] = ~UART_RX || ~UART_TX;
+  assign LED[2] = IO4;
+
+  always @(posedge clk_sys) clock_heartbeat <= clock_heartbeat +  25'd1;
 
   // Instantiating the Ibex Demo System.
   ibex_demo_system #(
-    .GpiWidth(3),
-    .GpoWidth(3),
-    .PwmWidth(12),
+    .GpiWidth(1),
+    .GpoWidth(1),
+    .PwmWidth(1),
     .SRAMInitFile(SRAMInitFile)
   ) u_ibex_demo_system (
     //input
     .clk_sys_i(clk_sys),
     .rst_sys_ni(rst_sys_n),
-    .gp_i({2'b00, SW}),
+    .gp_i(IO3),
     .uart_rx_i(UART_RX),
 
     //output
-    .gp_o(LED),
-    .pwm_o(RGB_LED),
+    .gp_o(IO4),
+    .pwm_o(),
     .uart_tx_o(UART_TX),
 
     .spi_rx_i(1'b0),
