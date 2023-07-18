7d9ad0995fd95b41eff673aadbfd554e67932d66
agamemnon <agamemnonaskyriazis@gmail.com>
2023-07-18 18:11:52 +0300

    [sw] edited common header files to support configurable uart parameters and updated the hello world program to demonstrate it

--- sw/c/demo/hello_world/main.c
+++ sw/c/demo/hello_world/main.c
@@ -1,30 +1,30 @@
 // Copyright lowRISC contributors.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
-
+#include "uart.h"
 #include "demo_system.h"
 #include "timer.h"
 #include "gpio.h"
 #include "pwm.h"
 #include <stdbool.h>
+#include <stdint.h>
 
 #define USE_GPIO_SHIFT_REG 0
 
 void test_uart_irq_handler(void) __attribute__((interrupt));
 
 void test_uart_irq_handler(void) {
-  int uart_in_char;
-
-  while ((uart_in_char = uart_in(DEFAULT_UART)) != -1) {
-    uart_out(DEFAULT_UART, uart_in_char);
-    uart_out(DEFAULT_UART, '\r');
-    uart_out(DEFAULT_UART, '\n');
-  }
+    unsigned char u;
+    u = getchar();
+    putchar(u);
 }
 
 int main(void) {
   install_exception_handler(UART_IRQ_NUM, &test_uart_irq_handler);
   uart_enable_rx_int();
+  
+  uart_enable(DEFAULT_UART, UART_RX_EN | UART_TX_EN);
+  uart_setup(DEFAULT_UART, DATA_SIZE_8 | PARITY_NONE | STOP_BITS_ONE | BAUD_RATE_115200);
 
   // This indicates how often the timer gets updated.
   timer_init();
@@ -41,37 +41,21 @@ int main(void) {
   bool ascending = true;
   // The three least significant bits correspond to RGB, where B is the leas significant.
   uint8_t color = 7;
-
+  
+  const char str[] = " Hello World ";
+  uint32_t c = 0;
   while(1) {
     uint64_t cur_time = get_elapsed_time();
 
     if (cur_time != last_elapsed_time) {
       last_elapsed_time = cur_time;
-
-      // Disable interrupts whilst outputting to prevent output for RX IRQ
-      // happening in the middle
+      
       set_global_interrupt_enable(0);
-
-      // Print this to UART (use the screen command to see it).
-      puts("Hello World! ");
-      puthex(last_elapsed_time);
-      puts("   Input Value: ");
-      uint32_t in_val = read_gpio(GPIO_IN_DBNC);
-      puthex(in_val);
-      putchar('\n');
-
-      // Re-enable interrupts with output complete
+      puts(str);
+      puthex(c++);
+      puts("\r\n");
       set_global_interrupt_enable(1);
-
-      // Cycling through green LEDs when BTN0 is pressed
-      if (USE_GPIO_SHIFT_REG) {
-        set_outputs(GPIO_OUT_SHIFT, in_val);
-      } else {
-        uint32_t out_val = read_gpio(GPIO_OUT);
-        out_val = ((out_val << 1) & GPIO_OUT_MASK) | (in_val & 0x1);
-        set_outputs(GPIO_OUT, out_val);
-      }
-
+      
       // Going from bright to dim on PWM
       for(int i = 0; i < NUM_PWM_MODULES; i++) {
         set_pwm(PWM_FROM_ADDR_AND_INDEX(PWM_BASE, i),
@@ -94,8 +78,8 @@ int main(void) {
           }
         }
       }
+ 
     }
-
     asm volatile ("wfi");
   }
 }
