ae020ab87573c7a9250f09f0a700ce323a1716eb
Marno van der Maas <mvdmaas+git@lowrisc.org>
2024-04-22 11:32:24 +0100

    Set objcopy to RISC-V

--- sw/c/gcc_toolchain.cmake
+++ sw/c/gcc_toolchain.cmake
@@ -1,6 +1,7 @@
 set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../common/link.ld")
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_C_COMPILER riscv32-unknown-elf-gcc)
+set(CMAKE_OBJCOPY riscv32-unknown-elf-objcopy)
 set(CMAKE_C_FLAGS_INIT
     "-march=rv32imc -mabi=ilp32 -mcmodel=medany -Wall -fvisibility=hidden -ffreestanding")
 set(CMAKE_ASM_FLAGS_INIT "-march=rv32imc")
