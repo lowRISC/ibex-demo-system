f80c19fadde1c68387b955854b126bd2f8ad8091
Marno van der Maas <mvdmaas+git@lowrisc.org>
2025-03-05 13:08:40 +0000

    OpenOCD: resolve deprecated command warnings
    The FTDI commands have changed and the ones used here were deprecated.

--- util/arty-a7-openocd-cfg.tcl
+++ util/arty-a7-openocd-cfg.tcl
@@ -5,10 +5,11 @@
 adapter driver ftdi
 transport select jtag
 
-ftdi_device_desc "Digilent USB Device"
-ftdi_vid_pid 0x0403 0x6010
-ftdi_channel 0
-ftdi_layout_init 0x0088 0x008b
+ftdi device_desc "Digilent USB Device"
+ftdi vid_pid 0x0403 0x6010
+ftdi channel 0
+ftdi layout_init 0x0088 0x008b
+
 reset_config none
 
 # Configure JTAG chain and the target processor
@@ -16,9 +17,9 @@ set _CHIPNAME riscv
 
 # Configure JTAG expected ID
 # arty-a7-35t
-set _EXPECTED_ID 0x0362D093 
+set _EXPECTED_ID 0x0362D093
 # arty-a7-100t
-# set _EXPECTED_ID 0x13631093 
+# set _EXPECTED_ID 0x13631093
 
 jtag newtap $_CHIPNAME cpu -irlen 6 -expected-id $_EXPECTED_ID -ignore-version
 set _TARGETNAME $_CHIPNAME.cpu
